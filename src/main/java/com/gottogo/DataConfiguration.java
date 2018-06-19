fe83476f33135883c08bfbb5de48eb9f8a4f0736
Jacson Pessoa <jacsonpessoa8@gmail.com>
2018-06-18 22:06:23 -0300

    nada
    nada

--- src/main/java/com/gottogo/DataConfiguration.java
+++ src/main/java/com/gottogo/DataConfiguration.java
@@ -15,9 +15,9 @@ public class DataConfiguration {
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:8000/auth");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/gottogo");
         dataSource.setUsername("root");
-        dataSource.setPassword("@Patrikfr15");
+        dataSource.setPassword("1234");
         return dataSource;
     }
 	
